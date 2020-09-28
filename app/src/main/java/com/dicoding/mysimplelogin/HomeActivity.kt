33bc39310e32d16c6f542cf963b2f89757ea0525
irshalfir <irshalfir@gmail.com>
2020-09-28 08:00:04 +0700

    implement chat feature at home activity

--- app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
+++ app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.dicoding.mysimplelogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.playground.ui.core.SessionManager
 import com.playground.ui.core.UserRepository
 import kotlinx.android.synthetic.main.activity_home.*
@@ -24,10 +25,22 @@ class HomeActivity : AppCompatActivity() {
             userRepository.logoutUser()
             moveToMainActivity()
         }
+
+        fab.setOnClickListener {
+            try {
+                moveToChatActivity()
+            } catch (e: Exception) {
+                Toast.makeText(this, "Module not found", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun moveToMainActivity() {
         startActivity(Intent(this, MainActivity::class.java))
         finish()
+    }
+
+    private fun moveToChatActivity() {
+        startActivity(Intent(this, Class.forName("com.dicoding.mysimplelogin.chat.ChatActivity")))
     }
 }
